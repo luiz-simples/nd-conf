8c3b05165815e4fbf1e45b082ff26c798d86de21
Luiz Amorim <luiz.simples@gmail.com>
2016-04-27 23:41:33 -0300

    build minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var uglify = require('gulp-uglify');
+//var sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("default", function () {
   return gulp.src("lib/**/*.js")
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(concat("nd-conf.js"))
-    .pipe(sourcemaps.write("."))
+    .pipe(uglify())
+    //.pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
