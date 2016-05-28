a7c3acd963d6049d1ec7fb97e30110c21c464e03
Luiz Amorim <luiz.simples@gmail.com>
2016-05-27 21:03:30 -0300

    change routes extension

--- tests/lib/ndcRoutes.test.js
+++ tests/lib/ndcRoutes.test.js
@@ -9,15 +9,15 @@ describe('ndcRoutes', () => {
     const pathRoutes = `${fixtures}/happy`
 
     return ndcRoutes(pathRoutes).then(routes => expect(routes).toEqual({
-      'GET: /end':   { verb: 'get', path: '/end',   file: `${fixtures}/happy/EndRest.json`   },
-      'GET: /start': { verb: 'get', path: '/start', file: `${fixtures}/happy/StartRest.json` },
+      'GET: /end':   { verb: 'get', path: '/end',   file: `${fixtures}/happy/EndRoutes.json`   },
+      'GET: /start': { verb: 'get', path: '/start', file: `${fixtures}/happy/StartRoutes.json` },
     }))
   })
 
   pit('verify duplicity of routes', () => {
     const pathRoutes = `${fixtures}/duplicity`
 
-    let expectMessage = `Duplicity of route: "GET: /end" on files: "${fixtures}/duplicity/StartRest.json" and "${fixtures}/duplicity/EndRest.json"`
+    let expectMessage = `Duplicity of route: "GET: /end" on files: "${fixtures}/duplicity/StartRoutes.json" and "${fixtures}/duplicity/EndRoutes.json"`
     let shouldReject = () => { throw new Error('should be rejected when duplicity of routes')}
 
     return ndcRoutes(pathRoutes)
